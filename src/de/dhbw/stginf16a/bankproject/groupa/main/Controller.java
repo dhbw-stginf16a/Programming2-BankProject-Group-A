99a5324336e29c24cbaf505673fd7c3cd5591834
Daniel Rutz <info@danielrutz.com>
2017-05-24 16:13:10 +0200

    Possible prototype: Hyperlink based Main Window

--- src/de/dhbw/stginf16a/bankproject/groupa/main/Controller.java
+++ src/de/dhbw/stginf16a/bankproject/groupa/main/Controller.java
@@ -1,7 +1,13 @@
 package de.dhbw.stginf16a.bankproject.groupa.main;
 
+import javafx.fxml.FXML;
+
 /**
  * Created by Jan-Robin Aumann on 23.05.2017.
  */
 public class Controller {
+    @FXML
+    protected void buttonPressed() {
+        System.exit(0);
+    }
 }
--- src/de/dhbw/stginf16a/bankproject/groupa/main/Main.java
+++ src/de/dhbw/stginf16a/bankproject/groupa/main/Main.java
@@ -1,7 +1,27 @@
 package de.dhbw.stginf16a.bankproject.groupa.main;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by Jan-Robin Aumann on 23.05.2017.
  */
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("AGROUP Bank System");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
