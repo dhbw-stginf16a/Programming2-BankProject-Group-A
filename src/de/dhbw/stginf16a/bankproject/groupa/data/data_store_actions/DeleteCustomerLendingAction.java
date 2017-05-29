569c93ab679fce7baa391372031f1f56f9de9e0d
Thore <thore@kruess.xyz>
2017-05-29 11:47:21 +0200

    Created DeleteCustomerLendingAction

---
+++ src/de/dhbw/stginf16a/bankproject/groupa/data/data_store_actions/DeleteCustomerLendingAction.java
@@ -0,0 +1,26 @@
+package de.dhbw.stginf16a.bankproject.groupa.data.data_store_actions;
+
+import de.dhbw.stginf16a.bankproject.groupa.data.BankDataStore;
+
+/**
+ * Created by thore on 5/29/17.
+ */
+public class DeleteCustomerLendingAction extends DataStoreAction {
+    private int customerId;
+    private int lendingId;
+
+    public DeleteCustomerLendingAction (int customerId, int lendingId) {
+        this.customerId = customerId;
+        this.lendingId = lendingId;
+    }
+
+    @Override
+    public BankDataStore apply(BankDataStore dataStore) throws DataStoreActionApplyException {
+        try {
+            dataStore.customers.get(customerId).lendings.remove(lendingId);
+        } catch (Exception e) {
+            throw new DataStoreActionApplyException("Error while deleting lending from customer.");
+        }
+        return dataStore;
+    }
+}
