f75bdb703b374157d98883143e8c7e8135eb0978
Jan-Robin Aumann <jan-robin@aumann.org>
2017-05-29 14:05:35 +0200

    changing some UI elements

--- src/de/dhbw/stginf16a/bankproject/groupa/client/main/MainSceneController.java
+++ src/de/dhbw/stginf16a/bankproject/groupa/client/main/MainSceneController.java
@@ -1,13 +1,24 @@
 package de.dhbw.stginf16a.bankproject.groupa.client.main;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+
+
 
 /**
  * Created by Jan-Robin Aumann on 23.05.2017.
  */
 public class MainSceneController {
+
     @FXML
-    protected void buttonPressed() {
-        System.exit(0);
+    private ListView listview;
+
+    @FXML
+    public void close(){
+        Stage stage = (Stage) listview.getScene().getWindow();
+
+        stage.close();
     }
 }
