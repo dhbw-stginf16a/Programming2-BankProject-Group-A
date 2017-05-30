3204f42fa39cfc80353f34e25b0f318b16add0f8
Daniel Rutz <info@danielrutz.com>
2017-05-30 10:24:31 +0200

    Make Gender look good again.

--- src/de/dhbw/stginf16a/bankproject/groupa/data/person_types/Gender.java
+++ src/de/dhbw/stginf16a/bankproject/groupa/data/person_types/Gender.java
@@ -4,5 +4,22 @@ package de.dhbw.stginf16a.bankproject.groupa.data.person_types;
  * Created by leons on 5/23/17.
  */
 public enum Gender {
-    MALE, FEMALE, APACHEATTACKHELICOPTER, OTHER
+    MALE, FEMALE, APACHEATTACKHELICOPTER, OTHER;
+
+    @Override
+    public String toString() {
+        switch (this) {
+
+            case MALE:
+                return "Male";
+            case FEMALE:
+                return "Female";
+            case APACHEATTACKHELICOPTER:
+                return "Apache Attack Helicopter";
+            case OTHER:
+                return "Other";
+            default:
+                return name();
+        }
+    }
 }
